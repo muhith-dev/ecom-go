f3c4eab45fccfd3af9b8d0f07287db3f6b16f9d7
yuzuteaaa <abdulmuhithnawawi@gmail.com>
2025-07-04 14:06:41 +0700

    HTTP Server setup

---
+++ cmd/api/api.go
@@ -0,0 +1,25 @@
+package api
+
+import (
+	"database/sql"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+type APIServer struct {
+	addr string
+	db   *sql.DB
+}
+
+func NewAPIServer(addr string, db *sql.DB) *APIServer {
+	return &APIServer{addr: addr, db: db}
+}
+
+func (s *APIServer) Run() error {
+	router := mux.NewRouter()
+	subrouter := router.PathPrefix("/api/v1").Subrouter()
+
+	log.Println("Listen on", s.addr)
+	return http.ListenAndServe(s.addr, router)
+}
