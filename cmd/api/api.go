117740137ddb69b43e38e2c1849e8d44f62e989c
yuzuteaaa <abdulmuhithnawawi@gmail.com>
2025-07-05 17:55:13 +0700

    Register user

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -21,7 +21,8 @@ func (s *APIServer) Run() error {
 	router := mux.NewRouter()
 	subrouter := router.PathPrefix("/api/v1").Subrouter()
 
-	userHandler := user.NewHandler()
+	userStore := user.NewStore(s.db)
+	userHandler := user.NewHandler(userStore)
 	userHandler.RegisterRoutes(subrouter)
 
 	log.Println("Listen on", s.addr)
--- service/user/routes.go
+++ service/user/routes.go
@@ -1,15 +1,20 @@
 package user
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
+	"github.com/muhith-dev/ecom-go/service/auth"
+	"github.com/muhith-dev/ecom-go/types"
+	"github.com/muhith-dev/ecom-go/utils"
 	"net/http"
 )
 
 type Handler struct {
+	store types.UserStore
 }
 
-func NewHandler() *Handler {
-	return &Handler{}
+func NewHandler(store types.UserStore) *Handler {
+	return &Handler{store: store}
 }
 
 func (h *Handler) RegisterRoutes(router *mux.Router) {
@@ -22,5 +27,29 @@ func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
+	var payload types.RegisterUserPayload
+	if err := utils.ParseJSON(r, payload); err != nil {
+		utils.WeriteError(w, http.StatusBadRequest, err)
+	}
+
+	_, err := h.store.GetUserByEmail(payload.Email)
+	if err == nil {
+		utils.WeriteError(w, http.StatusBadRequest, fmt.Errorf("user already exists", payload.Email))
+		return
+	}
+
+	hashedPassword, err := auth.HashPassword(payload.Password)
+
+	err = h.store.CreateUser(types.User{
+		FirstName: payload.FirstName,
+		LastName:  payload.LastName,
+		Email:     payload.Email,
+		Password:  hashedPassword,
+	})
+
+	if err != nil {
+		utils.WeriteError(w, http.StatusInternalServerError, err)
+		return
+	}
 
 }
